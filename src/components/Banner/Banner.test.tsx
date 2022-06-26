816baac6ba616cfee8407ed6771be77616240cbc
Nicholas Chumney <nicholas.chumney@outlook.com>
2022-06-26 15:09:18 -0400

    added text color prop

--- src/components/Banner/Banner.test.tsx
+++ src/components/Banner/Banner.test.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import Banner from '../Banner';
 
 describe('<Banner />', () => {
   test('render Banner', () => {
     render(<Banner src="" alt="banner" title="Test Title" />);
+    expect(screen.getByText('Test Title')).toBeVisible();
   });
 });
--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -9,22 +9,28 @@ export const Div = styled.div`
   position: relative;
 `;
 
-export const Title = styled.h3`
+interface TitleProps {
+  readonly color: string;
+}
+
+export const Title = styled.h3<TitleProps>`
   position: absolute;
   top: 120px;
+  color: ${(props) => props.color};
 `;
 
 export interface Props {
   src: string;
   alt: string;
   title: string;
+  color?: string;
 }
 
-const Banner = ({ src, alt, title }: Props) => {
+const Banner = ({ src, alt, title, color = 'black' }: Props) => {
   return (
     <Div>
       <img src={src} alt={alt} />
-      <Title>{title}</Title>
+      <Title color={color}>{title}</Title>
     </Div>
   );
 };
